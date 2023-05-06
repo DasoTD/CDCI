753e7d032b7a79c32999c2037dafe6f83da68190
David <david.daso@sterling.ng>
2023-05-06 22:06:04 +0100

    binary tree

---
+++ chapter4.py
@@ -0,0 +1,11 @@
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.right = None
+        self.left =  None
+
+class Treee:
+    def __init__(self, value):
+        new_node = Node(value);
+    pass
