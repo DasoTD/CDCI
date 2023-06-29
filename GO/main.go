7d69263be375f5b8df5e6a1a3d2a5b378f1e4e09
David <daso.timileyin@gmail.com>
2023-06-29 11:09:27 +0100

    go

--- GO/main.go
+++ GO/main.go
@@ -16,6 +16,15 @@ func main() {
 	for index, character := range(s){
 	  fmt.Printf("The character %c is in position %d \n", character, index)
 	}
+
+	first := map[string]int{"a": 1, "b": 2, "c": 3}
+	second := map[string]int{"a": 1, "e": 5, "c": 3, "d": 4}
+ 
+	for k, v := range second {
+		first[k] = v
+	}
+ 
+	fmt.Println(first)
 }
 
 
--- chapter1.py
+++ chapter1.py
@@ -296,3 +296,32 @@ runam = Chapter1()
 runam.specialMultiply("abcd")
 runam.stringCompress("aabbba")
 runam.zeroMatrix([[1,0,3],[4,5,6]])
+
+
+def areCharactersUnique(s):
+     
+    # An integer to store presence/absence
+    # of 26 characters using its 32 bits
+    checker = 0
+     
+    for i in range(len(s)):
+         
+        val = ord(s[i]) - ord('a')
+         
+        # If bit corresponding to current
+        # character is already set
+        if (checker & (1 << val)) > 0:
+            return False
+         
+        # set bit in checker
+        checker |= (1 << val)
+         
+    return True
+     
+# Driver code
+s = "aaabbccdaa"
+if areCharactersUnique(s):
+    print("Yes")
+else:
+    print("No")
+     
