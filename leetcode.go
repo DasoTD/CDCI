8e47217b2258060f3d5fb244620e5013288468d5
David <daso.timileyin@gmail.com>
2023-07-24 15:03:45 +0100

    threeSum

--- leetcode.go
+++ leetcode.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 
 func setDifference(a,b [] int) []int{
@@ -87,12 +90,70 @@ func isOpeningBracket(char rune) bool {
 }
 
 
+func maxProfit(prices []int) int {
+	n := len(prices)
+	if n <1{
+		return 0
+	}
+	buyday, sellday, maxProfit :=0, 1,0
+
+	for sellday <n{
+		profit := prices[sellday] - prices[buyday]
+		if profit > maxProfit{
+			maxProfit = profit
+		}
+		if prices[sellday]< prices[buyday]{
+			buyday = sellday
+		}
+		sellday++
+	}
+	return maxProfit
+}
+
+
+func threeSum(nums []int) [][]int {
+	n := len(nums)
+	sort.Ints(nums)
+	var result [][]int
+	for num1Idx := 0; num1Idx < n-2; num1Idx++ {
+		// Skip all duplicates from left
+		// num1Idx>0 ensures this check is made only from 2nd element onwards
+		if num1Idx > 0 && nums[num1Idx] == nums[num1Idx-1] {
+			continue
+		}
+		num2Idx := num1Idx + 1
+		num3Idx := n - 1
+		for num2Idx < num3Idx {
+			sum := nums[num2Idx] + nums[num3Idx] + nums[num1Idx]
+			if sum == 0 {
+				// Add triplet to result
+				result = append(result, []int{nums[num1Idx], nums[num2Idx], nums[num3Idx]})
+
+				num3Idx--
+
+				// Skip all duplicates from right
+				for num2Idx < num3Idx && nums[num3Idx] == nums[num3Idx+1] {
+					num3Idx--
+				}
+			} else if sum > 0 {
+				// Decrement num3Idx to reduce sum value
+				num3Idx--
+			} else {
+				// Increment num2Idx to increase sum value
+				num2Idx++
+			}
+		}
+	}
+	return result
+}
 
 var num2 = []int {2,4,6}
 var num1 = []int {1,2,3}
 var nums = [] int {2, 7, 11, 15}
+var dd = [] int {7,1,5,3,6,4}
 func main (){
 	findDifference(num1, num2)
 	fmt.Print(twoSums(nums, 9))
-	fmt.Print(lengthOfLongestSubstring("aabcdbcd"))
+	fmt.Println(lengthOfLongestSubstring("aabcdbcd"))
+	fmt.Println(maxProfit(dd))
 }
