a045d1640746c1ff42c09b96d7288347c7cd5334
David <daso.timileyin@gmail.com>
2023-12-15 10:11:51 -0800

    plate stacks

--- CDCI-GO/chapter3.go
+++ CDCI-GO/chapter3.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"container/list"
+	"errors"
+	_ "fmt"
+)
+
+
+
 type Stack2 struct {
 	value     []int
 
@@ -58,5 +66,106 @@ func (stack *StackMin) Pop() (int, bool){
 }
 
 
+////STACK OF PLATES
 
 
+type SetOfStacks struct {
+	capacity int           // Maximum capacity of each individual stack
+	stacks   *list.List    // List of stacks
+	current  *list.Element // Pointer to the current stack
+}
+
+
+type Stack struct {
+	items []int
+}
+
+
+func NewSetOfStacks(capacity int) *SetOfStacks {
+	return &SetOfStacks{
+		capacity: capacity,
+		stacks: list.New(),
+		current: nil,
+	}
+}
+
+
+func(sos *SetOfStacks) Push(value int) {
+	if sos.current == nil || sos.current.Value.(*Stack).Size() == sos.capacity {
+		newStack := NewStack()
+		sos.current = sos.stacks.PushBack(newStack)
+	}
+	sos.current.Value.(*Stack).Push(value)
+
+}
+
+func (s *SetOfStacks) pop() (int, error) {
+	if s.stacks.Len() == 0 {
+		return 0, errors.New("stack is empty")
+	}
+
+	value, err := s.current.Value.(*Stack).Pop()
+	if err != nil {
+		// Current stack is empty, remove it from the set of stacks
+		s.stacks.Remove(s.current)
+		if s.stacks.Len() > 0 {
+			// Update current stack to the last stack in the list
+			s.current = s.stacks.Back()
+		} else {
+			s.current = nil
+		}
+	}
+	return value, err
+}
+
+func (s *SetOfStacks) popAt(index int) (int, error) {
+	if index < 0 || index >= s.stacks.Len() {
+		return 0, errors.New("invalid stack index")
+	}
+
+	stackElement := s.stacks.Front()
+	for i := 0; i < index; i++ {
+		stackElement = stackElement.Next()
+	}
+
+	value, err := stackElement.Value.(*Stack).Pop()
+	if stackElement.Value.(*Stack).IsEmpty() {
+		// If the stack is empty, remove it from the set of stacks
+		s.stacks.Remove(stackElement)
+		if s.stacks.Len() > 0 {
+			// Update current stack to the last stack in the list
+			s.current = s.stacks.Back()
+		} else {
+			s.current = nil
+		}
+	}
+	return value, err
+}
+
+
+
+func NewStack() *Stack {
+	return &Stack{items: []int{}}
+}
+
+
+func(s *Stack) Push(item int){
+	s.items = append(s.items, item)
+}
+
+func (s *Stack) Pop() (int, error) {
+	if s.IsEmpty() {
+		return 0, errors.New("stack is empty")
+	}
+	value := s.items[len(s.items)-1]
+	s.items = s.items[:len(s.items)-1]
+	return value, nil
+}
+
+func (s *Stack) IsEmpty() bool {
+	return len(s.items) == 0
+}
+
+func (s *Stack) Size() int {
+	return len(s.items)
+}
